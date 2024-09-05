684f12ab6b270342af3213ef28be23adc354f19f
Evandro Antonio da Costa de Paula <eacpaula@outlok.com>
2024-09-04 21:27:34 -0300

    chapter1: First contact, learning how to configure the environment. Lesson 8: Create the first code "hello world", andlearn how to execute and compile it.

--- hello/main.go
+++ hello/main.go
@@ -4,6 +4,17 @@ import (
 	"fmt"
 )
 
+/*
+* Reference: Chapter 1 > Lesson 8
+*
+* To run this code:
+* - from vscode you can use this shortcut: ctrl+f5
+* - from terminal you can use this command line "go hello/main.go
+*
+* To compile this code:
+* - use terminal and run this command line "go build hello/main.go"
+ */
+
 func main() {
-	fmt.Println("Hello from Go!")
+	fmt.Println("Hello my brother! We're starting the work with Golang!")
 }
