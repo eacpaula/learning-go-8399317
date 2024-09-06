52acde70b66dd38fa3f0704d2d08498cb5889d95
Evandro Antonio da Costa de Paula <eacpaula@outlok.com>
2024-09-06 19:53:59 -0300

    Chapter 2 > Lesson 3 - We learned how to receive other tipes of variables from the terminal and how to convert it to correspondent type

--- practice/main.go
+++ practice/main.go
@@ -4,14 +4,14 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
 /*
  * Reference: Chapter 2 > Lesson 3
  *
- * We learned other two different libraries "bufio" and "os"
- *
- * We created a simple example about how to read informations into a terminal application
+ * We learned how to receive other tipes of variables from the terminal and how to convert it to correspondent type
  */
 
 func main() {
@@ -19,4 +19,13 @@ func main() {
 	fmt.Print("Enter text: ")
 	input, _ := reader.ReadString('\n')
 	fmt.Println("You entered: ", input)
+
+	fmt.Print("Enter a number: ")
+	numInput, _ := reader.ReadString('\n')
+	aFloat, err := strconv.ParseFloat(strings.TrimSpace(numInput), 64)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("Value of number: ", aFloat)
+	}
 }
