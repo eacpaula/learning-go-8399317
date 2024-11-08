068926a89efbe2abce0fe5bceb60633ea52249c6
Evandro Antonio da Costa de Paula <eacpaula@outlok.com>
2024-11-08 14:38:44 -0300

    Chapter 3 - Lesson 3 - Learning the basis about array declaration

--- practice/main.go
+++ practice/main.go
@@ -5,21 +5,23 @@ import (
 )
 
 /*
- * Reference: Chapter 3 > Lesson 2
+ * Reference: Chapter 3 > Lesson 3
  *
- * Learning how pointers works on go
+ * Store ordered values in array
  */
 
 func main() {
-	anInt := 47
-	var p = &anInt
-	fmt.Println("Value of p:", *p)
+	var colors [3]string
+	colors[0] = "Red"
+	colors[1] = "Green"
+	colors[2] = "Blue"
 
-	value1 := 42.13
-	pointer1 := &value1
-	fmt.Println("Value of pointer1:", *pointer1)
+	fmt.Println("The colors are:", colors)
+	fmt.Println("The first color is:", colors[0])
 
-	*pointer1 = *pointer1 / 31
-	fmt.Println("Value of pointer1:", *pointer1)
-	fmt.Println("Value of value1:", value1)
+	var numbers = [5]int{1, 2, 3, 4, 5}
+	fmt.Println("The numbers are:", numbers)
+
+	fmt.Println("Number of colors:", len(colors))
+	fmt.Println("Number of numbers:", len(numbers))
 }
