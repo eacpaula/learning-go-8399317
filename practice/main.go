0fbc0d5c4a43ff1a77f42db9a777f52526c5ccc1
Evandro Antonio da Costa de Paula <eacpaula@outlok.com>
2024-11-11 18:17:34 -0300

    Chapter 5 - Lesson 2 - Define functions as methods of custom types

--- practice/main.go
+++ practice/main.go
@@ -5,44 +5,51 @@ import (
 )
 
 /*
- * Reference: Chapter 5 > Lesson 1
+ * Reference: Chapter 5 > Lesson 2
  *
- * Define and call functions
+ * Define functions as methods of custom types
  */
 
 func main() {
-	doSomething()
-
-	result := addValues(1, 2)
-	fmt.Println(result)
-
-	total := addAllValues(1, 2, 3, 4, 5)
-	fmt.Println(total)
-
-	total, quantity := addAllValues2(1, 2, 3, 4, 5)
-	fmt.Println(total, quantity)
-}
-
-func doSomething() {
-	fmt.Println("Do something")
-}
-
-func addValues(a, b int) int {
-	return a + b
-}
-
-func addAllValues(values ...int) int {
-	total := 0
-	for _, value := range values {
-		total += value
+	dog := Dog{
+		Name:   "Fido",
+		Age:    10,
+		Breed:  "Poodle",
+		Weight: 45,
+		Sound:  "Woof",
 	}
-	return total
+
+	fmt.Println(dog)
+	fmt.Printf("%+v\n", dog)
+
+	fmt.Println(dog.Name)
+	fmt.Println(dog.Age)
+	fmt.Println(dog.Breed)
+	fmt.Println(dog.Weight)
+
+	dog.Bark()
+	dog.Sound = "Bark"
+	dog.Bark()
+
+	dog.Sound = "Arf!"
+	dog.BarkTrreeTimes()
 }
 
-func addAllValues2(values ...int) (total int, quantity int) {
-	for _, value := range values {
-		total += value
-	}
-	quantity = len(values)
-	return
+type Dog struct {
+	Name   string
+	Age    int
+	Breed  string
+	Weight int
+	Sound  string
+}
+
+// Bark is a method of the Dog type
+func (d Dog) Bark() {
+	fmt.Println(d.Sound)
+}
+
+// BarkTrreeTimes is a method of the Dog type
+func (d Dog) BarkTrreeTimes() {
+	d.Sound = fmt.Sprintf("%v %v %v", d.Sound, d.Sound, d.Sound)
+	fmt.Println(d.Sound)
 }
